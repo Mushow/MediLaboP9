ae32f372d2fe0a5b0787b2d3b1eaa15ea1d918a9
Antoine James <antoine@mushow.uk>
2024-05-16 14:18:43 +0200

    Add NoteRepository using MongoRepository

---
+++ medilabo.notes/src/main/java/uk/mushow/notes/repository/NoteRepository.java
@@ -0,0 +1,14 @@
+package uk.mushow.notes.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+import uk.mushow.notes.models.NotePatient;
+
+import java.util.List;
+
+@Repository
+public interface NoteRepository extends MongoRepository<NotePatient, Long> {
+
+    List<NotePatient> findByPatientId(Long patientId);
+
+}
