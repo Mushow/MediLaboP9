f27b9f99efc15bf89858d5418e2f6b94f82a0aa3
Antoine James <antoine@mushow.uk>
2024-05-16 14:17:59 +0200

    Added Exclude DataSource because IntelliJ refuses to start the APP

---
+++ medilabo.notes/src/main/java/uk/mushow/notes/MedilaboApplication.java
@@ -0,0 +1,14 @@
+package uk.mushow.notes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+public class MedilaboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MedilaboApplication.class, args);
+    }
+
+}
