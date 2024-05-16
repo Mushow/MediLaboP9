12e878d8096c72ad5a5c5c9f9bebc63572898261
Antoine James <antoine@mushow.uk>
2024-05-16 14:19:09 +0200

    Add NoteService

---
+++ medilabo.notes/src/main/java/uk/mushow/notes/service/NoteService.java
@@ -0,0 +1,41 @@
+package uk.mushow.notes.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import uk.mushow.notes.models.NotePatient;
+import uk.mushow.notes.repository.NoteRepository;
+
+import java.util.List;
+
+@Service
+public class NoteService {
+
+    @Autowired
+    private NoteRepository noteRepository;
+
+    public List<NotePatient> getNotesByPatient(Long patientId){
+        return noteRepository.findByPatientId(patientId);
+    }
+
+    public void deleteNoteByPatientId(Long patientId) {
+        List<NotePatient> allPatientsNotes = noteRepository.findByPatientId(patientId);
+        noteRepository.deleteAll(allPatientsNotes);
+    }
+
+    public NotePatient getNoteById (Long id) {
+        return noteRepository.findById(id).get();
+    }
+
+    public void createNote(NotePatient note){
+        noteRepository.insert(note);
+    }
+
+    public void updateNote(NotePatient note){
+        noteRepository.save(note);
+    }
+
+    public void deleteNoteById(Long id){
+        noteRepository.deleteById(id);
+    }
+
+}
