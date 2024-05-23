18bced8baa5da3fc6eb90e5952e31a437b580fe9
Mushow <antoine@mushow.uk>
2024-05-23 12:31:16 +0200

    Add ClientFront basics

---
+++ medilabo.client-front/src/main/java/uk/mushow/client/ClientFrontApplication.java
@@ -0,0 +1,13 @@
+package uk.mushow.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientFrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClientFrontApplication.class, args);
+    }
+
+}
---
+++ medilabo.client-front/src/main/java/uk/mushow/client/security/SecurityConfig.java
@@ -0,0 +1,50 @@
+package uk.mushow.client.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+    @Value("${spring.username}")
+    String username;
+    @Value("${spring.password}")
+    String password;
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+        http.csrf(ServerHttpSecurity.CsrfSpec::disable);
+        http
+                .authorizeExchange(exchanges -> {exchanges
+                        .pathMatchers("/**")
+                        .hasRole("ADMIN");
+                    exchanges.anyExchange().authenticated();}
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public MapReactiveUserDetailsService userDetailsService() {
+        UserDetails user = User.builder()
+                .username(username)
+                .password(passwordEncoder().encode( password))
+                .roles("ADMIN")
+                .build();
+        return new MapReactiveUserDetailsService(user);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
