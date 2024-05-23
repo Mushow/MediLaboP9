39517e3f5e014b442569dd976bc9b620f657202e
Mushow <antoine@mushow.uk>
2024-05-23 19:24:59 +0200

    Todo web proxy + templates

--- medilabo.client-front/src/main/java/uk/mushow/client/proxy/WebProxy.java
+++ medilabo.client-front/src/main/java/uk/mushow/client/proxy/WebProxy.java
@@ -5,6 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 @FeignClient(name = "gateway", url = "${gateway}")
 public class WebProxy {
 
-
+    //Stuff to do here
+    //Interact with other services
 
 }
