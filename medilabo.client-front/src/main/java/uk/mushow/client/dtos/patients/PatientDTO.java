94959c5d2a0f349497ba2f7231b435d70ef27668
Antoine James <antoine@mushow.uk>
2024-05-26 18:11:32 +0200

    Add format to birthDate

--- medilabo.client-front/src/main/java/uk/mushow/client/dtos/patients/PatientDTO.java
+++ medilabo.client-front/src/main/java/uk/mushow/client/dtos/patients/PatientDTO.java
@@ -1,12 +1,21 @@
 package uk.mushow.client.dtos.patients;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
 import java.util.Date;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PatientDTO {
 
     private Long id;
     private String name;
     private String surname;
+    @DateTimeFormat(pattern = "dd/MM/yyyy")
     private Date birthDate;
     private String gender;
     private String phoneNumber;
