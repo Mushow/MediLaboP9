07d4f33daa411ede5da911e233b3e3a09af9de78
Antoine James <antoine@mushow.uk>
2024-05-26 18:11:49 +0200

    Add lombok annotations

--- medilabo.client-front/src/main/java/uk/mushow/client/dtos/notes/NotePatientDTO.java
+++ medilabo.client-front/src/main/java/uk/mushow/client/dtos/notes/NotePatientDTO.java
@@ -1,5 +1,12 @@
 package uk.mushow.client.dtos.notes;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotePatientDTO {
 
     private Long noteId;
