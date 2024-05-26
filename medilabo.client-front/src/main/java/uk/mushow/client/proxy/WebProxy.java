c50760e1d8e9f2ab4b1b5c551399ab3299fa52fc
Antoine James <antoine@mushow.uk>
2024-05-26 16:44:17 +0200

    Added NotePatient & used FeignClient to interact with the gateway

--- medilabo.client-front/src/main/java/uk/mushow/client/proxy/WebProxy.java
+++ medilabo.client-front/src/main/java/uk/mushow/client/proxy/WebProxy.java
@@ -1,11 +1,30 @@
 package uk.mushow.client.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.*;
+import uk.mushow.client.dtos.notes.NotePatient;
 
-@FeignClient(name = "gateway", url = "${gateway}")
-public class WebProxy {
+import java.util.List;
 
-    //Stuff to do here
-    //Interact with other services
+@FeignClient(name = "gateway", url = "${gateway.url}")
+public interface WebProxy {
+
+    @GetMapping(value = "/note/patient/{id}")
+    List<NotePatient> getNotesByPatientId(@PathVariable("id") Long patientId);
+
+    @GetMapping(value = "/note/{id}")
+    NotePatient getNoteById(@PathVariable("id") Long id);
+
+    @PostMapping(value = "/note")
+    void createNote(@RequestBody NotePatient note);
+
+    @PutMapping(value = "/note")
+    void updateNote(@RequestBody NotePatient note);
+
+    @DeleteMapping(value = "/note/{id}")
+    void deleteNoteById(@PathVariable("id") Long id);
+
+    @DeleteMapping(value = "/note/patient/{id}")
+    void deleteNoteByPatientId(@PathVariable("id") Long patientId);
 
 }
