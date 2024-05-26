8d6423ca598b3d33e8aa7620f408f9e03de31cf3
Antoine James <antoine@mushow.uk>
2024-05-26 16:59:42 +0200

    Added PatientDTO & WebProxy routes to use it

---
+++ medilabo.client-front/src/main/java/uk/mushow/client/dtos/patients/PatientDTO.java
@@ -0,0 +1,15 @@
+package uk.mushow.client.dtos.patients;
+
+import java.util.Date;
+
+public class PatientDTO {
+
+    private Long id;
+    private String name;
+    private String surname;
+    private Date birthDate;
+    private String gender;
+    private String phoneNumber;
+    private String address;
+
+}
--- medilabo.client-front/src/main/java/uk/mushow/client/proxy/WebProxy.java
+++ medilabo.client-front/src/main/java/uk/mushow/client/proxy/WebProxy.java
@@ -2,7 +2,8 @@ package uk.mushow.client.proxy;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
-import uk.mushow.client.dtos.notes.NotePatient;
+import uk.mushow.client.dtos.notes.NotePatientDTO;
+import uk.mushow.client.dtos.patients.PatientDTO;
 
 import java.util.List;
 
@@ -10,21 +11,38 @@ import java.util.List;
 public interface WebProxy {
 
     @GetMapping(value = "/note/patient/{id}")
-    List<NotePatient> getNotesByPatientId(@PathVariable("id") Long patientId);
+    List<NotePatientDTO> getNotesByPatientId(@PathVariable("id") Long patientId);
 
     @GetMapping(value = "/note/{id}")
-    NotePatient getNoteById(@PathVariable("id") Long id);
+    NotePatientDTO getNoteById(@PathVariable("id") Long id);
 
     @PostMapping(value = "/note")
-    void createNote(@RequestBody NotePatient note);
+    void createNote(@RequestBody NotePatientDTO note);
 
     @PutMapping(value = "/note")
-    void updateNote(@RequestBody NotePatient note);
+    void updateNote(@RequestBody NotePatientDTO note);
 
     @DeleteMapping(value = "/note/{id}")
     void deleteNoteById(@PathVariable("id") Long id);
 
     @DeleteMapping(value = "/note/patient/{id}")
     void deleteNoteByPatientId(@PathVariable("id") Long patientId);
+
+
+
+    @GetMapping("/patients")
+    List<PatientDTO> getAllPatients();
+
+    @GetMapping("/patients/{id}")
+    PatientDTO getPatientById(@PathVariable("id") Long id);
+
+    @PostMapping("/patients")
+    PatientDTO createPatient(@RequestBody PatientDTO patientDTO);
+
+    @PutMapping("/patients")
+    PatientDTO updatePatient(@RequestBody PatientDTO patientDTO);
+
+    @DeleteMapping("/patients/{id}")
+    void deletePatientById(@PathVariable("id") Long id);
 
 }
