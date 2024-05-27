068193c3b7a13888628a36391d764fd108eb5682
Antoine James <antoine@mushow.uk>
2024-05-27 18:51:13 +0200

    Created TWO User

--- medilabo.client-front/src/main/java/uk/mushow/client/security/SecurityConfig.java
+++ medilabo.client-front/src/main/java/uk/mushow/client/security/SecurityConfig.java
@@ -1,5 +1,8 @@
 package uk.mushow.client.security;
 
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,6 +10,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +19,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+import java.io.IOException;
+import java.util.Set;
 
 @Configuration
 @EnableWebSecurity
@@ -25,14 +35,36 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(auth -> {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/login").permitAll();
                     auth.anyRequest().authenticated();
-                }).formLogin(form ->
-                        form.loginProcessingUrl("/login").defaultSuccessUrl("/patient", true))
+                })
+                .formLogin(form -> form.loginProcessingUrl("/login")
+                        .successHandler(customAuthenticationSuccessHandler())
+                )
                 .httpBasic(Customizer.withDefaults());
         return http.build();
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler customAuthenticationSuccessHandler() {
+        return new CustomAuthenticationSuccessHandler();
+    }
+
+    public static class CustomAuthenticationSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+        @Override
+        public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                            Authentication authentication) throws IOException, ServletException {
+            Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+            if (roles.contains("ROLE_DOCTOR")) {
+                getRedirectStrategy().sendRedirect(request, response, "/doctor");
+            } else if (roles.contains("ROLE_ORGANIZER")) {
+                getRedirectStrategy().sendRedirect(request, response, "/organizer");
+            } else {
+                super.onAuthenticationSuccess(request, response, authentication);
+            }
+        }
     }
 
     @Bean
